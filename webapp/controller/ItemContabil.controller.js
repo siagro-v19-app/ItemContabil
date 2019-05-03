c931a14da8ca60a1b7c161642ad98eee6b66fab0
matusca96 <gomesloud@gmail.com>
2019-05-03 10:51:32 -0300

    Atualização.

--- webapp/controller/ItemContabil.controller.js
+++ webapp/controller/ItemContabil.controller.js
@@ -15,19 +15,21 @@ sap.ui.define([
 			this.getOwnerComponent().setModel(oParamModel, "parametros");
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 			
-			this.getModel().attachMetadataLoaded(function(){
-				var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+			var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
 				var oView = this.getView();
 				var oTable = oView.byId("tableItemContabil");
-				var oColumn = oView.byId("columnDescricao");
 				
-				oTable.sort(oColumn);
-				oView.byId("tableItemContabil").getBinding("rows").filter(oFilter, "Application");
+			oTable.bindRows({
+				path: '/ItemContabils',
+				sorter: {
+					path: 'Descricao'
+				},
+				filters: oFilter
 			});
 		},
 		
 		filtraItem: function(oEvent){
-			var sQuery = oEvent.getParameter("query");
+			var sQuery = oEvent.getParameter("query").toUpperCase();
 			var oFilter1 = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
 			var oFilter2 = new Filter("Descricao", FilterOperator.Contains, sQuery);
 			
